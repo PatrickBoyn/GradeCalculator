874c0919ad6bcf44f4ec21101c7d1e60593de870
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-10 00:19:21 -0400

    Trying to work out how or statements work in Python.

--- grade_calc.py
+++ grade_calc.py
@@ -5,4 +5,9 @@ num_right = int(input("Please enter the number you got right: \n"))
 
 grade = num_right/num_questions * 100
 
-print("Your grade was " + str(grade))
+if grade >= 90:
+    print("Your grade was {} A".format(str(grade)))
+elif grade < 90 or grade >=80:
+    print("Your grade was {} B".format(str(grade)))
+elif grade < 80 or grade >=70:
+    print("Your grade was {} C".format(str(grade)))
