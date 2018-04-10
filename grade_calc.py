355c8ffc53663d10aa940088215b402eb1fb659e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-10 00:52:43 -0400

    The logic mostly works now with a nifty version of the or statement I found.

--- grade_calc.py
+++ grade_calc.py
@@ -2,12 +2,17 @@ num_questions = int(input("Please enter the number of questions: \n"))
 
 num_right = int(input("Please enter the number you got right: \n"))
 
-
+# Calculates the percent of a grade for a test. 
 grade = num_right/num_questions * 100
 
-if grade >= 90:
-    print("Your grade was {} A".format(str(grade)))
-elif grade < 90 or grade >=80:
-    print("Your grade was {} B".format(str(grade)))
-elif grade < 80 or grade >=70:
-    print("Your grade was {} C".format(str(grade)))
+# I have no idea if this is Python 3 or 2 code.
+# It works either way and I think it's easy to understand.
+if (100 <= grade >= 90):
+    print("Your score was {} A".format(str(grade)))
+elif (80 < grade > 70):
+    print("Your score was a {} B".format(str(grade)))
+elif (70 < grade > 60):
+    print("Your score was a {} C".format(str(grade)))
+elif (60 < grade > 50):
+    print("Your score was a {} D".format(str(grade)))
+    print("You did not pass.")
