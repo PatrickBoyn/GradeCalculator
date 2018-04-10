4c4660916c7cc2acb8b43b7477dc8c4c8f25e3ef
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-10 13:58:48 -0400

    Added a clearer statement for the percent at the end.

--- grade_calc.py
+++ grade_calc.py
@@ -11,13 +11,13 @@ grade = math.floor(round((num_right/num_questions * 100), 0))
 # I have no idea if this is Python 3 or 2 code.
 # It works either way and I think it's easy to understand.
 if (100 <= grade >= 90):
-    print("Your score was {} A".format(str(grade)))
+    print("Your score was {}% which is an A".format(str(grade)))
 elif (80 < grade > 70):
-    print("Your score was a {} B".format(str(grade)))
+    print("Your score was a {}% which is a B".format(str(grade)))
 elif (70 <= grade > 60):
-    print("Your score was a {} C".format(str(grade)))
+    print("Your score was a {}% which is a C".format(str(grade)))
 elif (60 <= grade > 50):
-    print("Your score was a {} D".format(str(grade)))
+    print("Your score was a {}% which is a D".format(str(grade)))
     print("You did not pass.")
 else:
     print("Your score was a {} F".format(str(grade)))
