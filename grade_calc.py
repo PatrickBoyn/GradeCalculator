5d8de7c19014aac34e7765196d766c34811c0084
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-09 21:47:49 -0400

    This is the basic logic. I will expand on it later.

--- grade_calc.py
+++ grade_calc.py
@@ -1,3 +1,8 @@
-input("Please enter the number of questions: \n")
+num_questions = int(input("Please enter the number of questions: \n"))
 
-input("Please enter the number you got wrong: \n")
+num_right = int(input("Please enter the number you got right: \n"))
+
+
+grade = num_right/num_questions * 100
+
+print("Your grade was " + str(grade))
